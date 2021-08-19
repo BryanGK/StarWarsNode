93af678aa25b10ee2ad5f1eb012831e4cd6fab34
BryanGK <bryangordonkrauss@gmail.com>
2021-08-19 14:20:06 -0700

    server now serves react app

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use('/', express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api', (req, res) => {
     res.json({ message: "Hello from Server" });
